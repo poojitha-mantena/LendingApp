26185be18e88107b95f1e3d8a96f4e50d2d8914c
H1t-esh <hitucool10@gmail.com>
2021-04-24 18:42:21 +0530

    PLEASE READ COMMIT CHANGE Google Sign In Authentication Done If in case not working for you,change the SHA1 certificate based on your android studio and update the same on firebase console project settings If you want to choose another google account to sign in after logout Uninstall and run the app again (WILL FIX THIS LATER)

--- app/src/main/java/com/example/lendingapp/Credentials/LoginActivity.java
+++ app/src/main/java/com/example/lendingapp/Credentials/LoginActivity.java
@@ -16,11 +16,18 @@ import android.widget.Toast;
 import com.example.lendingapp.Dashboard.SelectCategoryPage;
 import com.example.lendingapp.ForgotPassword;
 import com.example.lendingapp.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -30,6 +37,19 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     private FirebaseAuth mAuth;
     private ProgressBar progressBar;
+    private GoogleSignInClient mGoogleSignInClient;
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user=mAuth.getCurrentUser();
+        if(user!=null){
+            Intent intent=new Intent(getApplicationContext(),SelectCategoryPage.class);
+            startActivity(intent);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +72,66 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         forgotPassword=(TextView)findViewById(R.id.tvForgotPassword);
         forgotPassword.setOnClickListener(this);
 
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        // Build a GoogleSignInClient with the options specified by gso
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        findViewById(R.id.GoogleButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+
     }
+
+    int RC_SIGN_IN=69;
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Toast.makeText(this,"Email Sign In Failed, Try Again Later",Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent intent=new Intent(getApplicationContext(),SelectCategoryPage.class);
+                            startActivity(intent);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(LoginActivity.this, "Unable To Authenticate", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
     @Override
     public void onClick(View v){
         switch (v.getId()){
--- app/src/main/java/com/example/lendingapp/Dashboard/SelectCategoryPage.java
+++ app/src/main/java/com/example/lendingapp/Dashboard/SelectCategoryPage.java
@@ -6,18 +6,40 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
+import com.example.lendingapp.Credentials.LoginActivity;
 import com.example.lendingapp.Navigation.NavigationBar;
 import com.example.lendingapp.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SelectCategoryPage extends AppCompatActivity {
 
     private CardView card1;
+     Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_category_page);
+
+        logoutButton=findViewById(R.id.logout);
+
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent=new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
         getSupportActionBar().hide();
 
         card1 = (CardView) findViewById(R.id.vechileCard);
