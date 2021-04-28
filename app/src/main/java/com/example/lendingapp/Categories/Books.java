3a936a22a8e10464e557ee69d51b53b235f9a725
poojitha-mantena <52426390+poojitha-mantena@users.noreply.github.com>
2021-04-28 15:57:59 +0530

    added books adapter

--- app/src/main/java/com/example/lendingapp/Categories/Books.java
+++ app/src/main/java/com/example/lendingapp/Categories/Books.java
@@ -1,21 +1,41 @@
 package com.example.lendingapp.Categories;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AbsListView;
 
+import com.example.lendingapp.BooksAdapter;
+import com.example.lendingapp.Model.Model;
+import com.example.lendingapp.ModelBooks;
 import com.example.lendingapp.PostAdd.AdBookDetails;
 import com.example.lendingapp.R;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Books extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    BooksAdapter booksAdapter;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books);
         getSupportActionBar().setTitle("Books");
 
+        recyclerView=(RecyclerView)findViewById(R.id.books_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        FirebaseRecyclerOptions<ModelBooks> options = new FirebaseRecyclerOptions.Builder<ModelBooks>()
+                .setQuery(FirebaseDatabase.getInstance().getReference().child("Books"),ModelBooks.class)
+                .build();
+        
     }
+
+    
 }
