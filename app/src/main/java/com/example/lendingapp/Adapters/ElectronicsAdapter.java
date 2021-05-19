3950c69bee8a027045075d23c0c89454479db471
poojitha-mantena <52426390+poojitha-mantena@users.noreply.github.com>
2021-05-19 16:47:23 +0530

    added electronics adapter

--- app/src/main/java/com/example/lendingapp/Adapters/ElectronicsAdapter.java
+++ app/src/main/java/com/example/lendingapp/Adapters/ElectronicsAdapter.java
@@ -1,4 +1,69 @@
 package com.example.lendingapp.Adapters;
 
-public class ElectronicsAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.lendingapp.Model.Model;
+import com.example.lendingapp.R;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class ElectronicsAdapter extends RecyclerView.Adapter<ElectronicsAdapter.ElectronicsViewHolder> {
+
+    private Context mContext;
+    private List<Model> mUploads;
+
+    public ElectronicsAdapter(Context context, List<Model> uploads){
+        mContext = context;
+        mUploads = uploads;
+    }
+
+    @Override
+    public ElectronicsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(mContext).inflate(R.layout.product_item_list,parent,false);
+        return new ElectronicsViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ElectronicsAdapter.ElectronicsViewHolder holder, int position) {
+        Model uploadCurrent = mUploads.get(position);
+        //Glide.with(mContext).load(uploadCurrent.getImageUrl()).into(holder.imageView);
+        //Glide.with(mContext).load(mUploads.get(position).getImageUrl()).into(holder.imageView);
+        //Picasso.get().load(uploadCurrent.getImageUrl()).into(holder.imageView);
+        Picasso.get().load(uploadCurrent.getImageUrl()).fit().centerCrop()
+                .into(holder.electronicImage);
+        holder.electronicName.setText(uploadCurrent.getMtitle());
+        holder.electronicPrice.setText(uploadCurrent.getMprice());
+        holder.electronicLocation.setText(uploadCurrent.getMlocation());
+        holder.electronicDescription.setText(uploadCurrent.getMdescription());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUploads.size();
+    }
+
+    public class ElectronicsViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView electronicName, electronicPrice,electronicLocation,electronicDescription;
+        public ImageView electronicImage;
+
+        public ElectronicsViewHolder(View itemView) {
+            super(itemView);
+            electronicName=itemView.findViewById(R.id.itemName);
+            electronicPrice=itemView.findViewById(R.id.itemPrice);
+            electronicLocation=itemView.findViewById(R.id.itemLocation);
+            electronicDescription=itemView.findViewById(R.id.item_description);
+            electronicImage = itemView.findViewById(R.id.productImage);
+        }
+    }
 }
--- app/src/main/java/com/example/lendingapp/Categories/Electronics.java
+++ app/src/main/java/com/example/lendingapp/Categories/Electronics.java
@@ -1,17 +1,38 @@
 package com.example.lendingapp.Categories;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.lendingapp.Adapters.BooksAdapter;
+import com.example.lendingapp.Adapters.ElectronicsAdapter;
+import com.example.lendingapp.Model.Model;
 import com.example.lendingapp.PostAdd.AdElectronicsDetails;
 import com.example.lendingapp.PostAdd.AdHardwareDetails;
 import com.example.lendingapp.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Electronics extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    ElectronicsAdapter electronicsAdapter;
+    List<Model> list;
+    SwipeRefreshLayout swipeRefreshLayout;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +48,60 @@ public class Electronics extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        // ignore this portion for now its not complete
+
+        swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipeRefreshLayout);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+
+                // repeated the code here to get datas from database.
+//                database.addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                        for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+//                            Model books = dataSnapshot.getValue(Model.class);
+//                            list.add(books);
+//                        }
+//                        booksAdapter.notifyDataSetChanged();
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError error) {
+//
+//                    }
+//                });
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        // ignore until this point.
+
+        recyclerView=(RecyclerView)findViewById(R.id.books_recyclerview);
+        database= FirebaseDatabase.getInstance().getReference("Books");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list=new ArrayList<>();
+        electronicsAdapter=new ElectronicsAdapter(this,list);
+        recyclerView.setAdapter(electronicsAdapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Model books = dataSnapshot.getValue(Model.class);
+                    list.add(books);
+                }
+                electronicsAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
 
     }
 }
